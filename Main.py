765559bceedc482bdd0115b57db2127a91f703c1
BenHerbst <ben.herbst@web.de>
2021-11-21 01:41:30 +0100

    Some changes

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import math
+from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 
 window = tk.Tk()
 window.title("LoudReducer - 1.0")
@@ -6,8 +11,45 @@ window.geometry('500x500')
 
 reduceLoudSounds = tk.BooleanVar()
 
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+
+    # This source_app_user_model_id check and if statement is optional
+    # Use it if you want to only get a certain player/program's media
+    # (e.g. only chrome.exe's media not any other program's).
+
+    # To get the ID, use a breakpoint() to run sessions.get_current_session()
+    # while the media you want to get is playing.
+    # Then set TARGET_ID to the string this call returns.
+
+    current_session = sessions.get_current_session()
+    if current_session:  # there needs to be a media session running
+        if current_session.source_app_user_model_id == TARGET_ID:
+            info = await current_session.try_get_media_properties_async()
+
+            # song_attr[0] != '_' ignores system attributes
+            info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+            # converts winrt vector to list
+            info_dict['genres'] = list(info_dict['genres'])
+
+            return info_dict
+
+    # It could be possible to select a program from a list of current
+    # available ones. I just haven't implemented this here for my use case.
+    # See references for more information.
+    raise Exception('TARGET_PROGRAM is not the current media session')
+
 def print_selection():
-    print(reduceLoudSounds.get())
+    current_media_info = asyncio.run(get_media_info())
+    print(current_media_info)
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    # Get current volume 
+    currentVolumeDb = volume.GetMasterVolumeLevel()
+    volume.SetMasterVolumeLevel(currentVolumeDb - 10.0, None)
 
 reduceCheckBox = tk.Checkbutton(window, text='Reduce loud sounds',variable=reduceLoudSounds, onvalue=True, offvalue=False, command=print_selection)
 reduceCheckBox.pack()
