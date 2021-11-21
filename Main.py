90c0f38fa4cca61575aaa880af7922ae78f13658
BenHerbst <ben.herbst@web.de>
2021-11-21 01:25:11 +0100

    Finished ui

--- Main.py
+++ Main.py
@@ -3,4 +3,13 @@ import tkinter as tk
 window = tk.Tk()
 window.title("LoudReducer - 1.0")
 window.geometry('500x500')
+
+reduceLoudSounds = tk.BooleanVar()
+
+def print_selection():
+    print(reduceLoudSounds.get())
+
+reduceCheckBox = tk.Checkbutton(window, text='Reduce loud sounds',variable=reduceLoudSounds, onvalue=True, offvalue=False, command=print_selection)
+reduceCheckBox.pack()
+
 window.mainloop()
