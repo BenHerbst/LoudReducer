14da9f35ac4d38b8768ee66e5a15137b8a120cbb
BenHerbst <ben.herbst@web.de>
2021-11-21 01:54:05 +0100

    Changes

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from winrt.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager
 from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
-from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
+import asyncio
 
 window = tk.Tk()
 window.title("LoudReducer - 1.0")
@@ -11,38 +12,8 @@ window.geometry('500x500')
 
 reduceLoudSounds = tk.BooleanVar()
 
-async def get_media_info():
-    sessions = await MediaManager.request_async()
-
-    # This source_app_user_model_id check and if statement is optional
-    # Use it if you want to only get a certain player/program's media
-    # (e.g. only chrome.exe's media not any other program's).
-
-    # To get the ID, use a breakpoint() to run sessions.get_current_session()
-    # while the media you want to get is playing.
-    # Then set TARGET_ID to the string this call returns.
-
-    current_session = sessions.get_current_session()
-    if current_session:  # there needs to be a media session running
-        if current_session.source_app_user_model_id == TARGET_ID:
-            info = await current_session.try_get_media_properties_async()
-
-            # song_attr[0] != '_' ignores system attributes
-            info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
-
-            # converts winrt vector to list
-            info_dict['genres'] = list(info_dict['genres'])
-
-            return info_dict
-
-    # It could be possible to select a program from a list of current
-    # available ones. I just haven't implemented this here for my use case.
-    # See references for more information.
-    raise Exception('TARGET_PROGRAM is not the current media session')
 
 def print_selection():
-    current_media_info = asyncio.run(get_media_info())
-    print(current_media_info)
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
